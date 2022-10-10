546c6203bdfc62fdd65685ec75d21fb2e85436c9
sehirsig <sebastianhirsig@bluewin.ch>
2022-10-10 11:43:55 +0200

    Added: Aufgabe 1 Dateien

---
+++ src/Übungsblatt1/aqua/blatt1/common/msgtypes/HandoffRequest.java
@@ -0,0 +1,18 @@
+package Übungsblatt1.aqua.blatt1.common.msgtypes;
+
+import java.io.Serializable;
+
+import Übungsblatt1.aqua.blatt1.common.FishModel;
+
+@SuppressWarnings("serial")
+public final class HandoffRequest implements Serializable {
+	private final FishModel fish;
+
+	public HandoffRequest(FishModel fish) {
+		this.fish = fish;
+	}
+
+	public FishModel getFish() {
+		return fish;
+	}
+}
